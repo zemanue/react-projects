294acf1050beab44a26d21abda10b3530664ba9a
zemanue <manureco.97@gmail.com>
2025-08-13 12:23:01 +0200

    Add Header component and use in App
    Created a reusable Header component to display the page title and updated App.jsx to use this new component instead of a hardcoded h1 element.

--- admin-panel/src/App.jsx
+++ admin-panel/src/App.jsx
@@ -1,12 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <h1>Admin Panel</h1>;
+      <Header title="Admin Panel" />
     </>
   )
 }
