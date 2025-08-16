adca54966ac409019ab3059222374cba6db0751c
zemanue <manureco.97@gmail.com>
2025-08-16 22:30:12 +0200

    Add XProfile component and integrate into App
    Introduced a new XProfile component to display user profile information and updated App.jsx to render this component with sample data.

--- 01-components/src/App.jsx
+++ 01-components/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import XWhoToFollow from './components/XWhoToFollow'
 import XPost from './components/XPost'
+import XProfile from './components/XProfile'
 
 function App() {
 
@@ -25,21 +26,44 @@ function App() {
 
   return (
     <>
-      <h1>↓ X Components (no state) ↓</h1>
-      <h2>X Post</h2>
+      <h1>↓ X Components (static) ↓</h1>
+      
+      <h2>Who to follow card</h2>
+      <XWhoToFollow users={users} />
+      <br />
+      
+      <h2>Post</h2>
       <XPost
         name={users[0].name}
         username={users[0].username}
         date="2h"
-        content="¡Nuevo video en mi canal de YouTube! 🎥"
+        content="¡Estoy aprendiendo React! 🥳"
         image="https://i.ytimg.com/vi/7iobxzd_2wY/hqdefault.jpg"
         replies={12}
         reposts={4}
         likes={56}
         views={1000}
       />
-      <h2>Who to follow card</h2>
-      <XWhoToFollow users={users} />
+      <br />
+
+      <h2>Profile</h2>
+      <XProfile
+        name={users[1].name}
+        username={users[1].username}
+        banner="https://pbs.twimg.com/profile_banners/3057747517/1740491967/1080x360"
+        bio={`📚 Te enseño programación desde cero
+        💻 Software engineer | Freelance fullstack
+        ⭐️ GitHub Star | Microsoft MVP
+        🤘 Únete al campus de la comunidad: http://mouredev.pro`}
+        professionalCategory="Ciencia y tecnología"
+        location="Building software with love from Galicia (Spain) to the world" 
+        link="https://mouredev.pro/"
+        joinDate="Marzo 2015"
+        following={404}
+        followers="254,9 mil"
+        isFollowing={true}
+      />
+      <br />
     </>
 
   )
