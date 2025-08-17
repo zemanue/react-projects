a03e87982e1d2d0a60587e6dc4fc003c34bba344
zemanue <manureco.97@gmail.com>
2025-08-17 23:54:47 +0200

    Add board state and turn display to Tic Tac Toe app
    Introduced useState to manage the board and current turn. Added a Square component to render each cell and highlight the current player's turn. Updated board layout styling to use CSS grid.

--- 04-tic-tac-toe/src/App.jsx
+++ 04-tic-tac-toe/src/App.jsx
@@ -1,10 +1,48 @@
+import { useState } from 'react'
 import './App.css'
 
+const TURNS = {
+  X: 'X',
+  O: 'O',
+}
+
+const Square = ({ children, isSelected, updateBoard, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  )
+}
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
+  const [turn, setTurn] = useState(TURNS.X)
 
   return (
     <>
       <h1>Tic Tac Toe</h1>
+      <section className='board'>
+        {board.map((_, index) => {
+          return (
+            <Square
+              key={index}
+              index={index}
+            >
+              {board[index]}
+            </Square>
+          )
+        })}
+      </section>
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
     </>
   )
 }
