6b04bee2650d4d6bb86c389d4e86c4e627431b46
zemanue <manureco.97@gmail.com>
2025-08-20 18:32:04 +0200

    Add win detection and confetti to Tic Tac Toe game
    Implemented winner detection logic and a confetti effect using the canvas-confetti library when a player wins. Added a reset button to restart the game and updated styles for the reset button. Updated dependencies to include canvas-confetti.

--- 04-tic-tac-toe/src/App.jsx
+++ 04-tic-tac-toe/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import confetti from 'canvas-confetti'
 
 const TURNS = {
   X: 'X',
@@ -20,21 +21,63 @@ const Square = ({ children, isSelected, updateBoard, index }) => {
   )
 }
 
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null))
 
   const [turn, setTurn] = useState(TURNS.X)
 
+  const [winner, setWinner] = useState(null)
+
+  const checkWinner = (boardToCheck) => {
+    // Se hace una comprobación de cada combinación ganadora
+    for (const combination of WINNING_COMBINATIONS) {
+      const [a, b, c] = combination
+      // Si los valores de la combinación son iguales y no son null, se retorna el ganador
+      if (
+        boardToCheck[a] &&
+        boardToCheck[a] === boardToCheck[b] &&
+        boardToCheck[a] === boardToCheck[c]
+      ) {
+        return boardToCheck[a]
+      }
+    }
+    // Si no hay ganador, se retorna null
+    return null
+  }
+
   const updateBoard = (index) => {
     // Se hace una copia del tablero porque nunca se debe modificar el estado
     const newBoard = [...board]
-    if (!newBoard[index]) {
+    if (!newBoard[index] && !winner) {
       newBoard[index] = turn // Se añade X / O dependiendo del turno
       setBoard(newBoard)
-  
+
+      const winner = checkWinner(newBoard)
+      if (winner) {
+        confetti() // Efecto de confeti al ganar
+        setWinner(winner)
+        return
+      }
       const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
       setTurn(newTurn)
     }
+  }
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setWinner(null)
+    setTurn(TURNS.X)
   }
 
   return (
@@ -61,6 +104,23 @@ function App() {
           {TURNS.O}
         </Square>
       </section>
+      {winner !== null && (
+        <section className='winner'>
+          <div className='text'>
+            <h2>
+              {
+                winner === false ? 'Empate' : `El ganador es: `
+              }
+            </h2>
+            <header className='win'>
+              {winner && <Square>{winner}</Square>}
+            </header>
+            <footer>
+              <button onClick={resetGame} className='reset-btn'>Volver a jugar</button>
+            </footer>
+          </div>
+        </section>
+      )}
     </>
   )
 }
