20602fa2f5abc6728b6511118c44adcbe86b6215
zemanue <manureco.97@gmail.com>
2025-09-24 17:38:48 +0200

    Show cat image with first word from random fact
    Fetches a random cat fact and displays it. Also fetches and displays a cat image from cataas.com with the first word of the fact as text.

--- 05-fetch-tech-test/src/App.jsx
+++ 05-fetch-tech-test/src/App.jsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useState } from 'react'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+// const CAT_ENDPOINT_IMAGE = `https://cataas.com/cat/says/`
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
 export default function App() {
   const [fact, setFact] = useState('')
+  const [imageUrl, setImageUrl] = useState()
 
   useEffect(() => {
     fetch(CAT_ENDPOINT_RANDOM_FACT)
-      .then((res ) => res.json())
-      .then((data) => setFact(data.fact))
+      .then(res => res.json())
+      .then(data => {
+        const fact = data.fact
+        setFact(fact)
+
+        const firstWord = fact.split(' ')[0]
+
+        fetch(`${CAT_PREFIX_IMAGE_URL}/cat/says/${firstWord}?json=true`)
+          .then(res => res.json())
+          .then(res => {
+            console.log(res)
+            const url = res.url
+            setImageUrl(url)
+          })
+      })
       .catch((err) => console.log(err))
   }, [])
 
@@ -26,9 +42,8 @@ export default function App() {
         <li>Cat Images: https://cataas.com/cat/says/hello</li>
       </ul>
       <br />
-      <ol>
-        <li>Random Fact: {fact}</li>
-      </ol>
+      {fact && <p>Random Fact: {fact}</p>}
+      {imageUrl && <img src={imageUrl} alt={`Imagen de un gato con la primera palabra del hecho ${fact}`} />}
     </main>
   )
 }
