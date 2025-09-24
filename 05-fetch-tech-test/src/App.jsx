d2241738733e3767917ffbe453017ff931b4ebcf
zemanue <manureco.97@gmail.com>
2025-09-24 14:13:18 +0200

    Update README and App with project explanation and APIs
    Replaces the default README with a description of the Fetch Technical Test, including an explanation of the app's functionality and the APIs used. Updates the App component to display this information in the UI.

--- 05-fetch-tech-test/src/App.jsx
+++ 05-fetch-tech-test/src/App.jsx
@@ -3,8 +3,18 @@ import React from 'react'
 export default function App() {
 
   return (
-    <>
+    <main>
       <h1>Fetch Technical Test</h1>
-    </>
+      <p>Explicación:</p>
+      <ol>
+        <li>Recupera un hecho aleatorio de gatos</li>
+        <li>Muestra una imagen de un gato con la primera palabra del hecho</li>
+      </ol>
+      <p>APIS:</p>
+      <ul>
+        <li>Facts Random: https://catfact.ninja/fact</li>
+        <li>Cat Images: https://cataas.com/cat/says/hello</li>
+      </ul>
+    </main>
   )
 }
